28b2d9335e02681d4bce862d6d8edbe505293427
Bye-Shuvo <abuhasnatshuvo605@gmail.com>
2026-02-28 00:14:34 +0600

    Changed auth.route.js

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -6,4 +6,12 @@ router.post("/signup" , (req, res) => {
     res.send("Signup successful");
 });
 
+router.post("/login" , (req, res) => {
+    res.send("Login successful");
+});
+
+router.post("/logout" , (req, res) => {
+    res.send("Logout successful");
+});
+
 export default router;
