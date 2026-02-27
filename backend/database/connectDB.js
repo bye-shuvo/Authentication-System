ee4b397e36d936baf99ece92ac5d043296d0aeac
Bye-Shuvo <abuhasnatshuvo605@gmail.com>
2026-02-28 01:00:36 +0600

    Added database configuration file

---
+++ backend/database/connectDB.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+    try {
+       const connect = await mongoose.connect(process.env.MONGO_URI);
+       console.log("Connected to the mongo-host :" , connect.connection.host);
+    } catch (error) {
+        console.log("Error generated while connecting to database: ", error.message);
+        process.exit(1); // 1 -> exit with a failure code.
+    }
+}
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import authRoutes from './routes/auth.route.js'
+import dotenv from "dotenv";
+import { connectDB } from './database/connectDB.js';
+
+dotenv.config();
 const app = express();
 
 app.use("/v1/api/auth" , authRoutes);
 
 app.listen(3000 , () => {
     console.log("Server started at the port : " , 3000);
+    connectDB();
 })
