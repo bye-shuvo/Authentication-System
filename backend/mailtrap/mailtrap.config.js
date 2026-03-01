f3ee7aecdda1f02f737bf4b46908c8e0c1ba0225
Bye-Shuvo <abuhasnatshuvo605@gmail.com>
2026-03-01 23:00:00 +0600

    Initialize mailtrap service

---
+++ backend/mailtrap/mailtrap.config.js
@@ -0,0 +1,28 @@
+import { MailtrapClient } from 'mailtrap' ;
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const client = new MailtrapClient({
+  token: process.env.MAILTRAP_TOKEN,
+});
+
+const sender = {
+  email: "hello@demomailtrap.co",
+  name: "Mailtrap Test",
+};
+const recipients = [
+  {
+    email: "abuhasnatshuvo605@gmail.com",
+  }
+];
+
+client
+  .send({
+    from: sender,
+    to: recipients,
+    subject: "You are awesome!",
+    text: "Congrats for sending test email with Mailtrap!",
+    category: "Integration Test",
+  })
+  .then(console.log, console.error);
