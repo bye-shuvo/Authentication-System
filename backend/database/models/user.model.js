ae02283f27c1c37e64ca76c025cc7f941002f648
Bye-Shuvo <abuhasnatshuvo605@gmail.com>
2026-03-02 23:57:57 +0600

    Modified the user model schema for user verification confirmation

--- backend/database/models/user.model.js
+++ backend/database/models/user.model.js
@@ -23,6 +23,10 @@ const userSchema = new mongoose.Schema(
     resetPasswordTokenExpiresAt: Date,
     verificationToken: String,
     verificationTokenExpiresAt: Date,
+    isVerified : {
+      type : Boolean , 
+      default : false
+    }
   },
   { timestamps: true },
 );
